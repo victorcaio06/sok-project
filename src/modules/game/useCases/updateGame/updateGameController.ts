dc3a8f2eb1a7b67d09d498e8ee158f1643966214
Victor Caio <kaioaraujo426@gmail.com>
2022-12-05 09:34:02 -0300

    ERROR: update Game

--- src/modules/game/useCases/updateGame/updateGameController.ts
+++ src/modules/game/useCases/updateGame/updateGameController.ts
@@ -7,12 +7,12 @@ export default class UpdateGameController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const { description, additional, warning } = request.body;
-    console.log(`Aqui é o id: ${id}, já aqui é a data: ${description}`);
     await this.updateGameUseCase.execute(+id, {
       description,
       additional,
       warning,
     });
-    return response.status(204).json({});
+
+    return response.sendStatus(204);
   }
 }
--- src/modules/game/useCases/updateGame/updateGameUseCase.ts
+++ src/modules/game/useCases/updateGame/updateGameUseCase.ts
@@ -1,3 +1,4 @@
+import { Game } from '@prisma/client';
 import { Prisma } from '../../../../database';
 import UpdateGameDTO from '../../dto/update-game-dto';
 
@@ -5,9 +6,10 @@ export default class UpdateGameUseCase {
   async execute(
     id: number,
     { description, additional, warning }: UpdateGameDTO
-  ): Promise<void> {
-    const data = { description, additional, warning };
-    await Prisma.game.update({ where: { id }, data });
-    return;
+  ): Promise<void | Game> {
+    return await Prisma.game.update({
+      where: { id },
+      data: { description, additional, warning },
+    });
   }
 }
